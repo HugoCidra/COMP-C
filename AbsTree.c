f19875932403a01b2da5e0eb3c2d5d89aee5f90c
NasterWasTaken <118892748+NasterWasTaken@users.noreply.github.com>
2023-11-10 16:54:44 +0000

    Updated Tree Files
    Today when I walked into my economics class I saw something I dread every time I close my eyes. Someone had brought their new gaming laptop to class. The Forklift he used to bring it was still running idle at the back. I started sweating as I sat down and gazed over at the 700lb beast that was his laptop. He had already reinforced his desk with steel support beams and was in the process of finding an outlet for a power cable thicker than Amy Schumer's thigh. I start shaking. I keep telling myself I'm going to be alright and that there's nothing to worry about. He somehow finds a fucking outlet. Tears are running down my cheeks as I send my last texts to my family saying I love them. The teacher starts the lecture, and the student turns his laptop on. The colored lights on his RGB Backlit keyboard flare to life like a nuclear flash, and a deep humming fills my ears and shakes my very soul. The entire city power grid goes dark. The classroom begins to shake as the massive fans begin to spin. In mere seconds my world has gone from vibrant life, to a dark, earth shattering void where my body is getting torn apart by the 150mph gale force winds and the 500 decibel groan of the cooling fans. As my body finally surrenders, I weep, as my school and my city go under. I fucking hate gaming laptops.

--- AbsTree.c
+++ AbsTree.c
@@ -4,25 +4,12 @@ Node *add_node(char *info, int line, int column){
 
     Node *temp = (Node*) malloc(sizeof(Node));
     temp->info = info;
-    temp->added_info = NULL;
-    temp->child = NULL;
-    temp->brother = NULL;   
-    temp->line = line;
     temp->column = column;
+    temp->line = line;
+    temp->child = NULL;
 
     return temp;
 
-}
-
-void add_brother(Node* node1, Node* node2){
-
-    Node *temp;
-    temp = node1;
-
-    while(temp->brother != NULL){
-        temp = temp->brother;
-    }
-    temp->brother = node2;
 }
 
 void print_AbsTree(Node *node, int depth){
@@ -33,6 +20,20 @@ void print_AbsTree(Node *node, int depth){
         margin[i] = ".";
     }
     margin[depth * 2] = '\0';
-
     
+    printf("%s%s\n",dots, node->info);
+
+    if (node->child)
+        print_AbsTree(node->child, depth+1);
+
+    free(node);
+    
+}
+
+void freeAbsTree (Node* node){
+
+    if (node->child)
+        freeAbsTree(node->child);
+
+    free (Node);
 }
